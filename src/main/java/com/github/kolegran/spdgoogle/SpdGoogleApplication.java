bda47a6c16929298ecdd47dc87306b531b45ecc7
kolegran <kolegran@gmail.com>
2019-05-18 12:23:21 +0300

    Initial commit

---
+++ src/main/java/com/github/kolegran/spdgoogle/SpdGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.github.kolegran.spdgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpdGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpdGoogleApplication.class, args);
+	}
+
+}
