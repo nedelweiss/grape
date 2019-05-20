ef909aefbd9f8271ac89ea1272230c8d7c39f083
kolegran <kolegran@gmail.com>
2019-05-21 02:43:31 +0300

    Implemented searching indexes

--- src/main/java/com/github/kolegran/spdgoogle/index/IndexService.java
+++ src/main/java/com/github/kolegran/spdgoogle/index/IndexService.java
@@ -4,57 +4,81 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @Service
 public class IndexService {
     public void index(Map<String, ParsePageDto> pages) {
         try {
-            Directory memoryIndex = FSDirectory.open(Paths.get("home/mell/data"));
+            Directory memoryIndex = FSDirectory.open(Paths.get("/home/mell/data"));
             StandardAnalyzer analyzer = new StandardAnalyzer();
 
-            List<Document> documents = indexDocument(pages, analyzer, memoryIndex);
+            indexDocument(pages, analyzer, memoryIndex);
+            
+            List<Document> documents = searchIndex("body", "Huygens", analyzer, memoryIndex);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public List<Document> indexDocument(Map<String, ParsePageDto> pages,
+    public void indexDocument(Map<String, ParsePageDto> pages,
                               StandardAnalyzer analyzer,
-                              Directory memoryIndex) throws IOException{
+                              Directory memoryIndex) throws IOException {
 
         IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
         IndexWriter writer = new IndexWriter(memoryIndex, indexWriterConfig);
 
-        List<Document> documents = pages.entrySet().stream()
-                .map(entry -> {
-                    Document document = null;
-                    try {
-                        document = new Document();
+        for (Map.Entry<String, ParsePageDto> entry : pages.entrySet()) {
+            Document document = new Document();
 
-                        document.add(new TextField("url", entry.getKey(), Field.Store.YES));
-                        document.add(new TextField("title", entry.getValue().getTitle(), Field.Store.YES));
-                        document.add(new TextField("body", entry.getValue().getBody(), Field.Store.YES));
+            document.add(new TextField("url", entry.getKey(), Field.Store.YES));
+            document.add(new TextField("title", entry.getValue().getTitle(), Field.Store.YES));
+            document.add(new TextField("body", entry.getValue().getBody(), Field.Store.YES));
 
-                        writer.addDocument(document);
-                    } catch (IOException e) {
-                        e.getMessage();
-                    }
-                    return document;
-                })
-                .collect(Collectors.toList());
+            writer.addDocument(document);
+
+        }
+        writer.close();
+    }
+
+    public List<Document> searchIndex(String inField,
+                                      String q,
+                                      StandardAnalyzer analyzer,
+                                      Directory memoryIndex) {
+        List<Document> documents = new ArrayList<>();
+
+        try {
+            Query query = new QueryParser(inField, analyzer).parse(q);
+
+            IndexReader indexReader = DirectoryReader.open(memoryIndex);
+            IndexSearcher searcher = new IndexSearcher(indexReader);
+
+            TopDocs topDocs = searcher.search(query, 10);
+
+            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                System.out.println(scoreDoc);
+                documents.add(searcher.doc(scoreDoc.doc));
+            }
+        } catch (IOException | ParseException e) {
+            e.getMessage();
+        }
 
         return documents;
     }
-
 }
